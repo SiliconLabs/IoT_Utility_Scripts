6e6041279cba356285f52becef4d9bde08ecc876
Timotej Ecimovic <timotej.ecimovic@silabs.com>
2020-10-09 09:08:16 -0400

    Modify the token matching logic to deal with the _SINGLETON vs non singleton case.
    The logic added is:
       a.) If new token is _SINGLETON but the rest of the name matches to the old token, then code will be reused.
       b.) If new token is not a singleton, but rest of the name matches the old token, minus the _SINGLETON, then code will be reused.

--- token_preserver/token_preserver.js
+++ token_preserver/token_preserver.js
@@ -6,10 +6,8 @@
 const fs = require('fs')
 const readline = require('readline')
 
-
-
 function usage() {
-    console.log(`Usage: node token_preserver.js [ ARGS ]
+  console.log(`Usage: node token_preserver.js [ ARGS ]
 
 Mandatory arguments: 
    -o|-oldHeader <FILENAME>: specifies the name of the old generated header
@@ -17,6 +15,7 @@ Mandatory arguments:
 
 Optional arguments:
    -w|-writeTo <FILENAME>: specifies the name of the file to write output to. If missing, stdout is used.
+   -matchSingleton <0|1> : if 0, then don't match singletons, if 1 then match _SINGLETON with non singleton codes. Default is 1.
 
 Examples:
    Analyze old.h and new.h and write out modified new header to mod.h:
@@ -25,8 +24,8 @@ Examples:
    Analyze old.h and new.h and write resulting output to standard output:
       node token_preserver.js -o old.h -n new.h
 `)
-    
-    process.exit(0)
+
+  process.exit(0)
 }
 
 var oldHeader = null
@@ -36,138 +35,179 @@ var outputFile = null
 
 var next = 'none'
 
-process.argv.forEach(a => {
-    if ( next == 'old' ) {
-        oldHeader = a
-        next = 'none'
-    }
-    if ( next == 'new' ) {
-        newHeader = a
-        next = 'none'
-    }
-    if ( next == 'write' ) {
-        outputFile = a
-        next = 'none'
-    }
-    if ( a == '-oldHeader' || a == '-o') {
-        next = 'old'
-    }
-    if ( a == '-newHeader' || a == '-n' ) {
-        next = 'new'
-    }
-    if ( a == '-w' || a == '-writeTo' ) {
-        next = 'write'
-    }
+var matchSingleton = 1
+
+// Argument processing
+process.argv.forEach((a) => {
+  if (next == 'old') {
+    oldHeader = a
+    next = 'none'
+  }
+  if (next == 'new') {
+    newHeader = a
+    next = 'none'
+  }
+  if (next == 'write') {
+    outputFile = a
+    next = 'none'
+  }
+  if (next == 'matchSingleton') {
+    matchSingleton = parseInt(a)
+    next = 'none'
+  }
+  if (a == '-oldHeader' || a == '-o') {
+    next = 'old'
+  }
+  if (a == '-newHeader' || a == '-n') {
+    next = 'new'
+  }
+  if (a == '-w' || a == '-writeTo') {
+    next = 'write'
+  }
+  if (a == '-matchSingleton') {
+    next = 'matchSingleton'
+  }
 })
 
-if ( oldHeader == null || newHeader == null ) usage()
+if (oldHeader == null || newHeader == null) usage()
 
 var oldMap = {}
 var newMap = {}
 var newLines = []
 var filesDone = 0
 
+// Simple logging function.
 function note(msg) {
-    if ( outputFile != null )
-        console.log(msg)
+  if (outputFile != null) console.log(msg)
 }
 
+// Callback triggered when either of the files is finished.
+// First time around it just increases the counted, next
+// time around, it actually does the processing. This way,
+// it doesn't matter which file finished reading fist.
 function finishedFile() {
-    if ( filesDone == 0 ) {
-        filesDone++
-        return
-    }
+  if (filesDone == 0) {
+    filesDone++
+    return
+  }
 
-    // Get the biggest old value
-    var biggestValue = 0
-    for ( const creator in oldMap ) {
-        var c = parseInt(oldMap[creator], 16)
-        if ( c > biggestValue ) 
-            biggestValue = c
-    }
+  // Get the biggest old value
+  var biggestValue = 0
+  for (const creator in oldMap) {
+    var c = parseInt(oldMap[creator], 16)
+    if (c > biggestValue) biggestValue = c
+  }
 
-    // Ok, we're done parsing input files now. Let's analyze the keys.
-    note('Analyze tokens...')
-    for ( const creator in newMap ) {
-        const newValue = newMap[creator]
-        const oldValue = oldMap[creator]
+  // Ok, we're done parsing input files now. Let's analyze the keys.
+  note('Analyze tokens...')
+  for (const creator in newMap) {
+    const newValue = newMap[creator]
+    const oldValue = oldMap[creator]
 
-        if ( newValue == oldValue ) {
-            // Do nothing...
-            note(`  ${creator}: same creator code, stays unchanged.`)
-        } else if ( oldValue == undefined ) {
-            var newCreatorCode = (biggestValue+1)
-            biggestValue++
-            newMap[creator] = `0x${newCreatorCode.toString(16).toUpperCase()}`
-            note(`  ${creator}: new token. Ensure a new creator code: ${newMap[creator]}.`)
-        } else if ( oldValue != newValue ) {
-            newMap[creator] = oldValue
-            note(`  ${creator}: creator code has changed, rewriting it back to the same value: ${newValue} => ${oldValue}`)
+    if (newValue == oldValue) {
+      // Do nothing...
+      note(`  ${creator}: same creator code, stays unchanged.`)
+    } else if (oldValue == undefined) {
+      var bypass = false
+      if (matchSingleton == 1) {
+        // Special case: check for singletons.
+        if (
+          creator.endsWith('_SINGLETON') &&
+          creator.substring(0, creator.length - 10) in oldMap
+        ) {
+          note(
+            `  ${creator}: exists in the old map as non-singleton, code will be reused.`
+          )
+          newMap[creator] = oldMap[creator.substring(0, creator.length - 10)]
+          bypass = true
+        } else if (creator + '_SINGLETON' in oldMap) {
+          note(
+            `  ${creator}: exists in the old map as a singleton, code will be reused.`
+          )
+          newMap[creator] = oldMap[creator + '_SINGLETON']
+          bypass = true
         }
+      }
+      if (!bypass) {
+        var newCreatorCode = biggestValue + 1
+        biggestValue++
+        newMap[creator] = `0x${newCreatorCode.toString(16).toUpperCase()}`
+        note(
+          `  ${creator}: new token. Ensure a new creator code: ${newMap[creator]}.`
+        )
+      }
+    } else if (oldValue != newValue) {
+      newMap[creator] = oldValue
+      note(
+        `  ${creator}: creator code has changed, rewriting it back to the same value: ${newValue} => ${oldValue}`
+      )
     }
+  }
 
-    // Output function
-    var output
-    var outputStream
+  // Output function
+  var output
+  var outputStream
 
-    if ( outputFile == null ) {
-        output = (msg) => console.log(msg)
+  if (outputFile == null) {
+    output = (msg) => console.log(msg)
+  } else {
+    outputStream = fs.createWriteStream(outputFile)
+    output = (msg) => outputStream.write(`${msg}\n`)
+  }
+
+  newLines.forEach((l) => {
+    if (l.startsWith('#define CREATOR')) {
+      var s = l.split(' ')
+      output(`${s[0]} ${s[1]} ${newMap[s[1]]}`)
+    } else if (l.startsWith('#define NVM3KEY')) {
+      var s = l.split(' ')
+      var x = s[1]
+      var creator = x.replace('NVM3KEY', 'CREATOR')
+      output(`#define ${x} ( ${s[3]} | ${newMap[creator]} )`)
     } else {
-        outputStream = fs.createWriteStream(outputFile)
-        output = (msg) => outputStream.write(`${msg}\n`)
+      output(l)
     }
-    
-    newLines.forEach(l => {
-        if ( l.startsWith('#define CREATOR') ) {
-            var s = l.split(' ')
-            output(`${s[0]} ${s[1]} ${newMap[s[1]]}`)
-        } else if ( l.startsWith('#define NVM3KEY') ) {
-            var s = l.split(' ')
-            var x = s[1]
-            var creator = x.replace('NVM3KEY', 'CREATOR')
-            output(`#define ${x} ( ${s[3]} | ${newMap[creator]} )`)
-        } else {
-            output(l)
-        }
-    })
-    
-    note(`Writing resulting header to: ${outputFile}`)
-    if ( outputStream != null )
-      outputStream.end()
+  })
+
+  note(`Writing resulting header to: ${outputFile}`)
+  if (outputStream != null) outputStream.end()
 }
 
+// Create the interface for the old header reading...
 const oldHeaderRead = readline.createInterface({
-    input: fs.createReadStream(oldHeader),
-    console: false
+  input: fs.createReadStream(oldHeader),
+  console: false,
 })
-
-oldHeaderRead.on('line', l => {
-    if ( l.startsWith('#define CREATOR') ) {
-        var s = l.split(' ')
-        oldMap[s[1]] = s[2]
-    }
+// ... read it into the oldMap ...
+oldHeaderRead.on('line', (l) => {
+  if (l.startsWith('#define CREATOR')) {
+    var s = l.split(' ')
+    oldMap[s[1]] = s[2]
+  }
 })
-
+// ... and trigger finishedFile() when done.
 oldHeaderRead.on('close', () => {
-    note(`Reading old header: ${oldHeader} ...`)
-    finishedFile()
+  note(`Reading old header: ${oldHeader} ...`)
+  finishedFile()
 })
 
+// Create the interface for the new header reading...
 const newHeaderRead = readline.createInterface({
-    input: fs.createReadStream(newHeader),
-    console: false
+  input: fs.createReadStream(newHeader),
+  console: false,
 })
 
-newHeaderRead.on('line', l => {
-    newLines.push(l)
-    if ( l.startsWith('#define CREATOR') ) {
-        var s = l.split(' ')
-        newMap[s[1]] = s[2]
-    }
+// ... read it into the newMap ...
+newHeaderRead.on('line', (l) => {
+  newLines.push(l)
+  if (l.startsWith('#define CREATOR')) {
+    var s = l.split(' ')
+    newMap[s[1]] = s[2]
+  }
 })
 
+// ... and trigger finishedFile() when done.
 newHeaderRead.on('close', () => {
-    note(`Reading new header: ${newHeader} ...`)
-    finishedFile()
+  note(`Reading new header: ${newHeader} ...`)
+  finishedFile()
 })
-
